66d7bfe21ffd20252e20e88cea309b5217d7afa6
Hajime Terasawa <hajime-terasawa@c-fo.com>
2019-11-01 00:34:34 +0900

    :sparkles: impl debezium to kinesis sample

---
+++ src/main/java/com/terakoya76/debeziumsample/DebeziumSampleApplication.java
@@ -0,0 +1,278 @@
+package com.terakoya76.debeziumsample;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.json.JsonConverter;
+import org.apache.kafka.connect.source.SourceRecord;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.kinesis.producer.Attempt;
+import com.amazonaws.services.kinesis.producer.KinesisProducerConfiguration;
+import com.amazonaws.services.kinesis.producer.KinesisProducer;
+import com.amazonaws.services.kinesis.producer.UserRecordFailedException;
+import com.amazonaws.services.kinesis.producer.UserRecordResult;
+
+import com.google.common.collect.Iterables;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import io.debezium.config.Configuration;
+import io.debezium.connector.mysql.MySqlConnectorConfig;
+import io.debezium.embedded.EmbeddedEngine;
+import io.debezium.relational.history.MemoryDatabaseHistory;
+import io.debezium.util.Clock;
+
+@SpringBootApplication
+public class DebeziumSampleApplication implements ApplicationRunner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DebeziumSampleApplication.class);
+
+    private static final String APP_NAME = "kinesis";
+    private static final String KINESIS_REGION_CONF_NAME = "kinesis.region";
+
+    private EmbeddedEngine engine;
+    private final Configuration config;
+    private final JsonConverter valueConverter;
+    private final KinesisProducer producer;
+    private final ExecutorService callbackThreadPool = Executors.newCachedThreadPool();
+    // KinesisProducer.addUserRecord is asynchronous. A callback can be used to receive the results.
+    private final FutureCallback<UserRecordResult> callback = new FutureCallback<UserRecordResult>() {
+        @Override
+        public void onFailure(Throwable t) {
+            // If we see any failures, we will LOGGER them.
+            if (t instanceof UserRecordFailedException) {
+                Attempt last = Iterables.getLast(
+                        ((UserRecordFailedException) t).getResult().getAttempts());
+                LOGGER.error(String.format(
+                        "Record failed to put - %s : %s",
+                        last.getErrorCode(), last.getErrorMessage()));
+            }
+            LOGGER.error("Exception during put", t);
+        }
+
+        @Override
+        public void onSuccess(UserRecordResult result) {
+            LOGGER.info("Successed: {}", result);
+        }
+    };
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebeziumSampleApplication.class, args);
+	}
+
+    public DebeziumSampleApplication() {
+        /*
+        config = Configuration.empty().withSystemProperties(Function.identity()).edit()
+                .with(EmbeddedEngine.CONNECTOR_CLASS, "io.debezium.connector.mysql.MySqlConnector")
+                .with(EmbeddedEngine.ENGINE_NAME, APP_NAME)
+                .with(MySqlConnectorConfig.SERVER_NAME,APP_NAME)
+                .with(MySqlConnectorConfig.SERVER_ID, 8192)
+
+                // for demo purposes let's store offsets and history only in memory
+                .with(EmbeddedEngine.OFFSET_STORAGE, "org.apache.kafka.connect.storage.MemoryOffsetBackingStore")
+                .with(MySqlConnectorConfig.DATABASE_HISTORY, MemoryDatabaseHistory.class.getName())
+
+                // Send JSON without schema
+                .with("schemas.enable", false)
+                .build();
+        */
+
+         config = Configuration.create()
+                .with(EmbeddedEngine.CONNECTOR_CLASS, "io.debezium.connector.mysql.MySqlConnector")
+                .with(EmbeddedEngine.ENGINE_NAME, "kinesis")
+                .with(MySqlConnectorConfig.SERVER_NAME, "kinesis")
+                .with(MySqlConnectorConfig.SERVER_ID, 8192)
+                .with(MySqlConnectorConfig.HOSTNAME, "localhost")
+                .with(MySqlConnectorConfig.PORT, 3306)
+                .with(MySqlConnectorConfig.USER, "debezium")
+                .with(MySqlConnectorConfig.PASSWORD, "dbz")
+                .with(MySqlConnectorConfig.DATABASE_WHITELIST, "inventory")
+                .with(MySqlConnectorConfig.TABLE_WHITELIST, "inventory.customers")
+                .with(EmbeddedEngine.OFFSET_STORAGE,
+                    "org.apache.kafka.connect.storage.MemoryOffsetBackingStore")
+                .with(MySqlConnectorConfig.DATABASE_HISTORY,
+                    MemoryDatabaseHistory.class.getName())
+                .with("schemas.enable", false)
+                .build();
+
+        valueConverter = new JsonConverter();
+        valueConverter.configure(config.asMap(), false);
+
+        producer = KinesisProducer("ap-northeast-1");
+    }
+
+    /**
+     * Here'll walk through some of the config options and create an instance of
+     * KinesisProducer, which will be used to put records.
+     *
+     * @param region The region of the Kinesis stream being used.
+     *
+     * @return KinesisProducer instance used to put records.
+     */
+    public KinesisProducer KinesisProducer(final String region) {
+        // There are many configurable parameters in the KPL. See the javadocs
+        // on each each set method for details.
+        KinesisProducerConfiguration config = new KinesisProducerConfiguration();
+
+        // You can also load config from file. A sample properties file is
+        // included in the project folder.
+        // KinesisProducerConfiguration config =
+        //     KinesisProducerConfiguration.fromPropertiesFile("default_config.properties");
+
+        // If you're running in EC2 and want to use the same Kinesis region as
+        // the one your instance is in, you can simply leave out the region
+        // configuration; the KPL will retrieve it from EC2 metadata.
+        config.setRegion(region);
+
+        // You can pass credentials programmatically through the configuration,
+        // similar to the AWS SDK. DefaultAWSCredentialsProviderChain is used
+        // by default, so this configuration can be omitted if that is all
+        // that is needed.
+        config.setCredentialsProvider(new ProfileCredentialsProvider("default"));
+
+        // The maxConnections parameter can be used to control the degree of
+        // parallelism when making HTTP requests. We're going to use only 1 here
+        // since our throughput is fairly low. Using a high number will cause a
+        // bunch of broken pipe errors to show up in the logs. This is due to
+        // idle connections being closed by the server. Setting this value too
+        // large may also cause request timeouts if you do not have enough
+        // bandwidth.
+        config.setMaxConnections(1);
+
+        // Set a more generous timeout in case we're on a slow connection.
+        config.setRequestTimeout(60000);
+
+        // RecordMaxBufferedTime controls how long records are allowed to wait
+        // in the KPL's buffers before being sent. Larger values increase
+        // aggregation and reduces the number of Kinesis records put, which can
+        // be helpful if you're getting throttled because of the records per
+        // second limit on a shard. The default value is set very low to
+        // minimize propagation delay, so we'll increase it here to get more
+        // aggregation.
+        config.setRecordMaxBufferedTime(2000);
+
+        config.setKinesisEndpoint("localhost");
+        config.setKinesisPort(4567);
+        config.setVerifyCertificate(false);
+
+        // If you have built the native binary yourself, you can point the Java
+        // wrapper to it with the NativeExecutable option. If you want to pass
+        // environment variables to the executable, you can either use a wrapper
+        // shell script, or set them for the Java process, which will then pass
+        // them on to the child process.
+        // config.setNativeExecutable("my_directory/kinesis_producer");
+
+        // If you end up using the default configuration (a Configuration instance
+        // without any calls to set*), you can just leave the config argument
+        // out.
+        //
+        // Note that if you do pass a Configuration instance, mutating that
+        // instance after initializing KinesisProducer has no effect. We do not
+        // support dynamic re-configuration at the moment.
+        KinesisProducer producer = new KinesisProducer(config);
+
+        return producer;
+    }
+
+    @Override
+    public void run(ApplicationArguments arg0) throws Exception {
+        engine = EmbeddedEngine.create()
+                .using(config)
+                .using(this.getClass().getClassLoader())
+                .using(Clock.SYSTEM)
+                .notifying(this::sendRecord)
+                .build();
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(engine);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            LOGGER.info("Requesting embedded engine to shut down");
+            engine.stop();
+        }));
+
+        // the submitted task keeps running, only no more new ones can be added
+        executor.shutdown();
+
+        awaitTermination(executor);
+
+        cleanUp();
+
+        LOGGER.info("Engine terminated");
+    }
+
+    private void awaitTermination(ExecutorService executor) {
+        try {
+            while (!executor.awaitTermination(10, TimeUnit.SECONDS)) {
+                LOGGER.info("Waiting another 10 seconds for the embedded engine to complete");
+            }
+        }
+        catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void cleanUp() {
+        producer.flushSync();
+        producer.destroy();
+    }
+
+    private void sendRecord(SourceRecord record) {
+        // We are interested only in data events not schema change events
+        if (record.topic().equals(APP_NAME)) {
+            return;
+        }
+
+        Schema schema = null;
+
+        if ( null == record.keySchema() ) {
+            LOGGER.error("The keySchema is missing. Something is wrong.");
+            return;
+        }
+
+        // For deletes, the value node is null
+        if ( null != record.valueSchema() ) {
+            schema = SchemaBuilder.struct()
+                    .field("key", record.keySchema())
+                    .field("value", record.valueSchema())
+                    .build();
+        } else {
+            schema = SchemaBuilder.struct()
+                    .field("key", record.keySchema())
+                    .build();
+        }
+
+        Struct message = new Struct(schema);
+        message.put("key", record.key());
+
+        if ( null != record.value() )
+            message.put("value", record.value());
+
+        String partitionKey = String.valueOf(record.key() != null ? record.key().hashCode() : -1);
+        final byte[] payload = valueConverter.fromConnectData("dummy", schema, message);
+
+        // TIMESTAMP is our partition key
+        ListenableFuture<UserRecordResult> f =
+                producer.addUserRecord(record.topic(), partitionKey, ByteBuffer.wrap(payload));
+        Futures.addCallback(f, callback, callbackThreadPool);
+    }
+
+    private String streamNameMapper(String topic) {
+        return topic;
+    }
+}
---
+++ src/test/java/com/terakoya76/debeziumsample/DebeziumSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terakoya76.debeziumsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebeziumSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
